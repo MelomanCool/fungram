909b4f4f4a257ebeda9ee134444fceed8be223aa
MelomanCool <melomancool@gmail.com>
2018-01-23 02:46:26 +0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='fungram',
+    version='0.1',
+    packages=['fungram', 'fungram.filters'],
+    author='MelomanCool',
+    author_email='melomancool@gmail.com',
+    url='https://github.com/MelomanCool/fungram',
+    install_requires=['python-telegram-bot', 'future']
+)
