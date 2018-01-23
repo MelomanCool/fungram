9233868a7c979b60146654b02b1d1c689a3dc3f0
MelomanCool <melomancool@gmail.com>
2018-01-24 02:52:16 +0400

    Compatibility with Python 3.4

--- fungram/filters/operators.py
+++ fungram/filters/operators.py
@@ -1,13 +1,13 @@
 def f_and(a, b, *preds):
     def inner(*args, **kwargs):
-        return all(pred(*args, **kwargs) for pred in (a, b, *preds))
+        return all(pred(*args, **kwargs) for pred in (a, b) + preds)
 
     return inner
 
 
 def f_or(a, b, *preds):
     def inner(*args, **kwargs):
-        return any(pred(*args, **kwargs) for pred in (a, b, *preds))
+        return any(pred(*args, **kwargs) for pred in (a, b) + preds)
 
     return inner
 
